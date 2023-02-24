581372440300861d9e49bacf6b59cac299aad14b
Tatyana58 <tatbir@rambler.ru>
2023-02-24 23:04:25 +0300

    Начало Задание №1

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,38 @@ Predicate корректно реализован через лямбда-выр
 
  */
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("--------------Задание №1------------- ");
+        System.out.println("--------------через анонимный класс------------- ");
+        // Создаем список
+        List<Integer> meaning = new ArrayList<>();
+
+        // Наполняем список
+        meaning.add(50);
+        meaning.add(43);
+        meaning.add(-8);
+        meaning.add(13);
+        meaning.add(-73);
+
+//        // Создаем объект, реализующий функциональный интерфейс Predicate,
+//        // и переопределяем его метод test() так, чтобы он возвращал true,
+//        // если >=0 иначе False
+        Predicate<Integer> predicate = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer meaning) {
+                return meaning >= 0;
+            }
+        };
+        for (int i=0; i< meaning.size();i++) {
+            System.out.println(predicate.test(meaning.get(i)));
+        }
+
+        System.out.println("--------------Задание №2------------- ");
+        System.out.println("--------------через лямбду------------- ");
     }
 }
